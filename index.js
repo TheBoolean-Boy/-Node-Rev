4a037021acedfb5e0e6a31214687a2f8af471e29
TheBoolean-Boy <saswatrath02@gmail.com>
2025-08-25 22:44:07 +0530

    Embedded Javascript

--- index.js
+++ index.js
@@ -5,32 +5,11 @@ const app = express();
 app.use(express.json());
 const PORT = 3000;
 
-//Our Middleware
-app.use("/welcome", (req, res, next) => {
-  console.log('Start'+new Date().toLocaleString());
-  res.on('finish', () => {
-    console.log("Finished" +new Date().toLocaleString());
-  })
-  next();
-})
-
-app.get("/welcome", (req, res) => {
-  console.log("Processing"+new Date().toLocaleString());
-  res.send("This is the Welcome Route");
-})
-
-app.get("/error", () => {
-  throw new Error("This is a test Error")
-})
-
-app.use((err, req, res, next) => {
-  console.log(err.message)
-  res.send("Error Handling Middleware Testing")
-})
+app.set('view engine', 'ejs')
 
 app.get("/", (req, res) => {
-  console.log("Parent Route"+new Date().toLocaleString());
-  res.send("Welcome to ExpressJS");
+  const username = "Saswat Rath"
+  res.render('pela', {username});
 })
 
 app.listen(PORT, () => {
