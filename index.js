be1882193c8e76c30d563b323c67d0ef273e10a2
TheBoolean-Boy <saswatrath02@gmail.com>
2025-08-26 16:02:51 +0530

    Wrapped up

--- index.js
+++ index.js
@@ -1,47 +1,17 @@
 import express from 'express'
-import { connectDB } from './cofig/db.js';
-import { Person } from './models/person.model.js';
-
 
 const app = express();
-
-
 app.use(express.json());
 const PORT = 3000;
 
-app.post("/person", async(req,res) => {
-
-  const {email, name, age} = req.body;
-  const newPerson = new Person({
-    name,
-    age,
-    email
-  })
-  await newPerson.save();
-
-  console.log(newPerson)
-  res.send("Person Added")
-})
-
-app.put("/person", async(req, res) => {
-  const {id} = req.body;
-  const personData = await Person.findById(id)
-  personData.age = 89;
-  await personData.save()
-  console.log(personData)
-  res.send("User Info Updated")
-})
-
-app.delete("/person/:id", async (req, res) => {
-  const {id} = req.params;
-  await Person.findByIdAndDelete(id)
-  res.send("Person details removed from db")
+app.get("/", (req, res) => {
+  res.send("Welcome to ExpressJS")
 })
 
 
 
 app.listen(PORT, async () => {
-  await connectDB();
+  // await connectDB();
   console.log(`Server is live at https://localhost:${PORT}`)
 })
 
